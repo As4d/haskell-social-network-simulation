b45f828543eace391b4d717478c7e1c26751b73e
Asad <asadkhan91003@gmail.com>
2025-12-18 04:18:30 +0000

    Implement user creation functionality and update package dependencies

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main (main) where
 
 import Types
+import Control.Concurrent.MVar
+
+createUser :: Int -> IO User
+createUser uid = do
+    emptyInbox <- newMVar []      
+    counter <- newMVar 0          
+    return User
+        { userId = uid
+        , username = "User" ++ show uid
+        , inbox = emptyInbox
+        , messageCount = counter
+        }
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Creating User Test"
+    
+    user1 <- createUser 1
+
+    putStrLn $ "Example: " ++ username user1
