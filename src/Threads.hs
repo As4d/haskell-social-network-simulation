8c0986ecfae35c0600d892f0943b93a2bc282945
Asad <asadkhan91003@gmail.com>
2025-12-18 22:16:49 +0000

    Enhance documentation for Threads module with detailed function descriptions and parameters

--- src/Threads.hs
+++ src/Threads.hs
@@ -1,3 +1,12 @@
+-- |
+-- Module      : Threads
+-- Description : User thread behavior for the social network simulation
+-- License     : BSD-3-Clause
+-- Maintainer  : asadkhan91003@gmail.com
+--
+-- This module defines the behavior of individual user threads in the concurrent
+-- social network simulation. Each user thread operates independently, sending
+-- messages to randomly selected recipients at random time intervals.
 module Threads (userThread) where
 
 import Control.Concurrent
@@ -6,7 +15,25 @@ import Types
 import User
 import Utils
 
--- What each user thread does
+-- | Defines the behavior of a single user thread in the simulation.
+--
+-- Each user thread performs the following actions:
+--
+-- 1. Waits for a random delay between 100ms and 500ms
+-- 2. Selects a random recipient from all other users (excluding itself)
+-- 3. Sends a message to the selected recipient
+--
+--
+-- ==== __Parameters__
+--
+-- [@thisUser@] The user represented by this thread
+-- [@allUsers@] List of all users in the simulation (including this user)
+--
+-- ==== __Thread Safety__
+--
+-- This function is designed to be called concurrently from multiple threads.
+-- All message sending operations are thread-safe through the use of MVars
+-- in the 'sendMessage' function.
 userThread :: User -> [User] -> IO ()
 userThread thisUser allUsers = do
   -- Random delay between 100ms and 500ms
