c675c82fae325e24d83c00504703d5d2170a1661
Asad <asadkhan91003@gmail.com>
2025-12-18 22:18:28 +0000

    Refactor Utils module documentation and formatting for clarity and consistency

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,22 +1,56 @@
+-- |
+-- Module      : Utils
+-- Description : Utility functions for the social network simulation
+-- License     : BSD-3-Clause
+-- Maintainer  : asadkhan91003@gmail.com
+--
+-- This module provides general utility functions used throughout the social
+-- network simulation, including random selection and message generation.
 module Utils
-    ( selectRandom
-    , getRandomMessage
-    ) where
+  ( selectRandom,
+    getRandomMessage,
+  )
+where
 
 import System.Random
 
--- Pick a random element from a list
+-- | Selects a random element from a non-empty list.
+--
+-- This function uses 'randomRIO' to generate a random index and returns
+-- the element at that position. It is used for randomly selecting message
+-- recipients and message content.
+--
+--
+-- ==== __Examples__
+--
+-- >>> selectRandom [1, 2, 3, 4, 5]
+-- 3  -- (result will vary, any element from the list)
+--
+-- ==== __Parameters__
+--
+-- [@xs@] A non-empty list to select from
 selectRandom :: [a] -> IO a
 selectRandom xs = do
-    index <- randomRIO (0, length xs - 1)
-    return (xs !! index)
+  index <- randomRIO (0, length xs - 1)
+  return (xs !! index)
 
--- Get a random message content
+-- | Generates random message content for user communications.
+--
+-- Returns one of several predefined casual messages at random. This
+-- simulates the random nature of social network messages in the simulation.
+--
+-- The current message pool includes: \"Hello\", \"Hi\", \"Yo\", \"Bye\", \"Cya\"
+--
+-- ==== __Examples__
+--
+-- >>> getRandomMessage
+-- "Hello"  -- (result will vary, one of the predefined messages)
 getRandomMessage :: IO String
-getRandomMessage = selectRandom
-    [ "Hello"
-    , "Hi"
-    , "Yo"
-    , "Bye"
-    , "Cya"
+getRandomMessage =
+  selectRandom
+    [ "Hello",
+      "Hi",
+      "Yo",
+      "Bye",
+      "Cya"
     ]
